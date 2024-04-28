3e089f63c777b24a3351467822fd65e5b444218a
readytotest <117216467+readytotest@users.noreply.github.com>
2024-04-28 10:05:23 -0700

    Add describe block and before each

--- tests/readyToTestSite.spec.ts
+++ tests/readyToTestSite.spec.ts
@@ -1,19 +1,26 @@
 //First sample test with Playwright
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://readytotest.github.io');
 
+test.describe('Ready to Test', () => {
+
+  test.beforeEach(async ({ page }) => {
+    console.log(`Running ${test.info().title}`);
+    await page.goto('https://readytotest.github.io');
+  });
+
+test('has title', async ({ page }) => {
   // Expect a title "to contain" a string.
   await expect(page).toHaveTitle('Ready to Test (QA)');
 });
 
+//go to weather alert page
 test('weather api link', async ({ page }) => {
-  await page.goto('https://readytotest.github.io');
-
   // Click the get weather API link.
   await page.getByRole('link', { name: 'View weather alerts on my page' }).click();
 
   // Expects page to have text.
   await expect(page.getByText('Current watches, warnings, and advisories for California')).toBeVisible();
+});
+
 });
