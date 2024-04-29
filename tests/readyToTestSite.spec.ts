ec835dad618a2757c6b9b9830b0ccc77cfd95cc6
readytotest <117216467+readytotest@users.noreply.github.com>
2024-04-28 17:08:46 -0700

    Add loop to run test 10 times & add unique ID to message

--- tests/readyToTestSite.spec.ts
+++ tests/readyToTestSite.spec.ts
@@ -1,9 +1,10 @@
 //First sample test with Playwright
 const { test } = require('@playwright/test');
 import { IndexHtmPage } from "../pages/index-htm-page";
+import { testIdGenerator } from "../scripts/testIdGenerator";
 
-
-
+//Run the test 10 times
+for (let i = 0; i <= 9; i++) {
 test.describe('My personal home page test suite', () => {
   let indexHtmPage; 
   
@@ -13,20 +14,20 @@ test.describe('My personal home page test suite', () => {
     await indexHtmPage.goto();
   });
   
-test('website title', async ({ page }) => {
+test(`website title run:${i}`, async ({ page }) => {
   //Verify title of index.htm
   await indexHtmPage.verifyPageTitle();
 });
 
 
-test('weather api', async ({ page }) => {
+test(`weather api run:${i}`, async ({ page }) => {
   //Go to weather alert page
   await indexHtmPage.clickWeatherAlertLink();
   //Expects page to display weather alerts for California
   await indexHtmPage.verifyWeatherAlertIsCalifornia();
 });
 
-test('feedback widget', async ({ page }) => {
+test(`feedback widget run:${i}`, async ({ page }) => {
   //Open the feedback widget
   await indexHtmPage.clickFeedbackWidget();
   //Fill in name field
@@ -43,3 +44,4 @@ test('feedback widget', async ({ page }) => {
 });
 
 });
+}
