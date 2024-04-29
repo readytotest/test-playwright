2307054f7ac42a85740e96278fc5600771c0d17c
readytotest <117216467+readytotest@users.noreply.github.com>
2024-04-28 17:06:00 -0700

    Add test id generator

---
+++ scripts/testIdGenerator.ts
@@ -0,0 +1,11 @@
+/*Using Date.now() to create random numbers
+Unix time: time in seconds since 1970/01/01
+This is an easy way to create unique numbers
+Google for a Unix Timestamp Epoch Converter 
+to convert the timestamp to your local time*/
+
+const ezRandomNum = Date.now();
+
+const testIdGenerator = ezRandomNum;
+
+export { testIdGenerator };
