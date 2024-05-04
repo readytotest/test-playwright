4854b952030b0b0c01bf7733c9aa4b039c2a5b70
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-04 09:13:11 -0700

    add weather alert page object

---
+++ pages/weather-alert-page.ts
@@ -0,0 +1,17 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class IndexHtmPage {
+  readonly page: Page;
+  readonly weatherAlertPageCopy: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.weatherAlertPageCopy = page.locator('weather-title');
+  }
+
+  //Verify we are displaying alerts for California
+  async verifyWeatherAlertIsCalifornia() {
+    await expect(this.weatherAlertPageCopy).toHaveText('current watches, warnings, and advisories for california', { ignoreCase: true });
+  }
+
+}
