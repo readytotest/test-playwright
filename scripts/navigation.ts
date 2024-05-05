ba022f2bf2c35a7232263cb6a406ccea04e002cf
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-05 16:44:37 -0700

    Add comment

--- scripts/navigation.ts
+++ scripts/navigation.ts
@@ -1,6 +1,12 @@
 import { Page } from '@playwright/test';
 
-//URL to start test from
+/*I have this setup to test against a local server
+  in the GitHub runner in a GitHub Actions workflow
+  in my primary repo that tests commits and pull requests.
+  There is no local server script in this repo so the
+  tests will fail, unless you change the websiteUrl
+  variable to be https://readytotest.github.io */
+
 const websiteUrl = 'http://localhost:3000';
 
 async function goToIndexHtm(page: Page) {
