11e74c355c8f5c61dbfa1e3985172a15a5ca77f3
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-07 16:27:14 -0700

    Update comment

--- scripts/navigation.ts
+++ scripts/navigation.ts
@@ -3,8 +3,11 @@ import { Page } from '@playwright/test';
 /*I have this setup to test against a local server
   in the GitHub runner in a GitHub Actions workflow
   in my primary repo that tests commits and pull requests.
-  If you want to run the specs on your computer, you must
-  change the websiteUrl variable to https://readytotest.github.io */
+  If you want to run the specs on your computer, the easiest
+  thing to do is change the websiteUrl variable to https://readytotest.github.io 
+  or else you will need to also clone my main repo, open both repos
+  in VSCode, start the server in the main repo and then run the
+  test */
 
 const websiteUrl = 'http://localhost:3000';
 
