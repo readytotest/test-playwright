b24c3fd0846383abd87acd28ae5935523a45d392
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-11 07:55:17 -0700

    Remove sloMo for the GitHub actions config

--- playwright.actions.config.ts
+++ playwright.actions.config.ts
@@ -23,11 +23,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    
-    /*Slow the tests down so I can watch them and see what's happening */
-    launchOptions: {
-       slowMo: 2000,
-     },
 
     //Show/hide the browser while running the test
       headless: true,
