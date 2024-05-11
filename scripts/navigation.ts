4bf05c34a7c443e76c345acd1921fe3bdacc4f82
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-11 11:28:50 -0700

    Add separate variables for environments and update instructions

--- scripts/navigation.ts
+++ scripts/navigation.ts
@@ -1,18 +1,20 @@
 import { Page } from '@playwright/test';
 
-/*I have this setup to test against a local server
-  in the GitHub runner in a GitHub Actions workflow
-  in my primary repo that tests commits and pull requests.
-  If you want to run the specs on your computer, the easiest
-  thing to do is change the websiteUrl variable to https://readytotest.github.io 
-  or else you will need to also clone my main repo, open both repos
-  in VSCode, start the server in the main repo and then run the
-  test */
+/*
+  This setup is configured to test against a local server in the GitHub runner
+  in a GitHub Actions workflow in the primary repo. If you want to run the specs
+  on your computer, you can change the variable from localHost to production.
+  If you do want to run the tests against a local environment, you will need to
+  clone the main repo, open both repos in VSCode, start the server in the main repo,
+  and then run the test.
+*/
 
-const websiteUrl = 'http://localhost:3000';
+const localHost = 'http://localhost:3000';
+const production = 'https://readytotest.github.io';
 
 async function goToIndexHtm(page: Page) {
-    await page.goto(websiteUrl);
+  //Swap out the localHost variable below with production if you want to test the live site
+    await page.goto(localHost);
 }
 
 export { goToIndexHtm };
