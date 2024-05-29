b217bf3af67e3a5cf5c1466c1c006423a444ad49
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-29 06:26:39 -0700

    Add testIgnore to ci Playwright config for the alternate spec, to avoid running duplicate tests in GH Actions

--- playwright.actions.config.ts
+++ playwright.actions.config.ts
@@ -11,6 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  testIgnore: '**/indexHtmFunction.spec.ts',  // Exclude this specific file in any subdirectory
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
