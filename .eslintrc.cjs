38b479719da9cd1bb8988d8066157f96832e953f
readytotest <117216467+readytotest@users.noreply.github.com>
2024-07-13 12:02:39 -0700

    chore: install husky and lint staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,16 @@
-module.exports = {
-    extends: ['eslint:recommended', 
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
-    'prettier'
-  ],
+  module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier',
+    ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
     parserOptions: {
-      project: true,
+      project: './tsconfig.json',
       tsconfigRootDir: __dirname,
     },
   };
+  
