f5ddfb0398774b2d4bf02b22fd66f3faa61434f9
readytotest <117216467+readytotest@users.noreply.github.com>
2024-07-13 10:31:46 -0700

    chore: install ESlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['eslint:recommended', 
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+  ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: __dirname,
+    },
+  };
