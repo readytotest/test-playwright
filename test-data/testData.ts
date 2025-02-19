c03cd9d1279cf99f313fce3eb932c60a5f038e53
readytotest <117216467+readytotest@users.noreply.github.com>
2025-02-19 13:46:35 -0800

    test: update json to accomodate new page copy verification

--- test-data/testData.ts
+++ test-data/testData.ts
@@ -7,7 +7,7 @@ const testData = {
   },
   indexHtmData: {
     pageTitle: "Ready to Test (QA)",
-    introPageCopy: "This is my personal homepage where I've been tinkering",
+    introPageCopy: "Welcome to my personal homepage, where I tinker with HTML,",
     introPageCopy2: "Curious about the design choice of this page?",
     weatherAlertLinkText: "View weather alerts on my page",
     weatherAlertHref: "/html/weather-api.htm",
