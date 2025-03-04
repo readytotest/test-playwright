bfce7ac3683299297df4f3817efcd6db704e075e
readytotest <117216467+readytotest@users.noreply.github.com>
2025-03-03 17:03:15 -0800

    chore: remove .eslintignore and update ESLint config to new style

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,15 +1,43 @@
-module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  root: true,
-  parserOptions: {
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
+module.exports = [
+  {
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      "eslint:recommended": "error",
+    },
   },
-};
+  {
+    files: ["*.ts", "*.tsx"],
+    languageOptions: {
+      plugins: ["@typescript-eslint"],
+    },
+    rules: {
+      "@typescript-eslint/recommended": "error",
+      "@typescript-eslint/recommended-requiring-type-checking": "error",
+    },
+  },
+  {
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/",
+      "test-results/",
+      "playwright-report",
+      "summary.json",
+      ".vscode/*",
+      ".DS_Store",
+      "Thumbs.db",
+      "*_spec3.json",
+    ],
+  },
+];
