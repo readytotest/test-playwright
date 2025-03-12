3c768934801e3831dedb0b953674439496cdf128
readytotest <117216467+readytotest@users.noreply.github.com>
2025-03-12 14:17:18 -0700

    test: add in fixed wait due to laggy weather api response

--- pages/weatherAlertPageObject.ts
+++ pages/weatherAlertPageObject.ts
@@ -17,8 +17,10 @@ import { expect, Page } from "@playwright/test";
 
 export const weatherAlertPageObject = (page: Page) => {
   const verifyWeatherTitle = async (title: string, options: { ignoreCase: boolean }) => {
-    // One day I had to put a 10 second fixed wait here due to the Weather API lag (not just in Playwright but using CURL as well)
-    // The API was slow to respond and the assertion was failing because the title was not being displayed
+    // I had to put a 10 second fixed wait here due to the Weather API lag (not just in Playwright but using CURL as well)
+    // Lately their API is slow to respond....
+    // And assertion was failing because the title was not being displayed
+    await page.waitForTimeout(10000);
     await expect(page.locator("weather-title")).toHaveText(title, options);
   };
 
