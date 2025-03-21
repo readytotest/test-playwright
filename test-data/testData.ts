1027d2cf2c58f3ed3e344dcdbdc29f011d2962bd
readytotest <117216467+readytotest@users.noreply.github.com>
2025-03-21 09:08:39 -0700

    test: update intro data due to expected change so assertion will pass

--- test-data/testData.ts
+++ test-data/testData.ts
@@ -7,7 +7,7 @@ const testData = {
   },
   indexHtmData: {
     pageTitle: "Ready to Test (QA)",
-    introPageCopy: "Welcome to my personal homepage, where I think about QA, tinker",
+    introPageCopy: "Welcome to my personal homepage. It's like a peek into my garage",
     introPageCopy2: "Curious about the design choice of this page?",
     weatherAlertLinkText: "View weather alerts on my page",
     weatherAlertHref: "/html/weather-api.htm",
