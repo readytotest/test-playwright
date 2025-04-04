e3311f3749fca9f6f0dcca5132f66f2444077ac8
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-04 08:19:31 -0700

    chore: set retries to 1 for github actions config file

--- playwright.actions.config.ts
+++ playwright.actions.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Do not retest on failure */
-  retries: 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
