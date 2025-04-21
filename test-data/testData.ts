904456567ce12fa556830dbf33428501e6c040aa
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-20 18:22:15 -0700

    chore: update test data to fix assertion string  due to expected changes and a broken test

--- test-data/testData.ts
+++ test-data/testData.ts
@@ -7,7 +7,7 @@ const testData = {
   },
   indexHtmData: {
     pageTitle: "Ready to Test (QA)",
-    introPageCopy: "Welcome to my personal homepage. It's like a peek into my garage",
+    introPageCopy: "Welcome to my half-QA blog, half-digital garage. It's like a peek into",
     introPageCopy2: "Curious about the design choice of this page?",
     weatherAlertLinkText: "View weather alerts on my page",
     weatherAlertHref: "/html/weather-api.htm",
