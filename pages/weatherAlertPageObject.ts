1c10ea71f5d8ec74c2ef03e32398ac48f624599e
Eli <117216467+readytotest@users.noreply.github.com>
2025-05-02 14:09:09 -0700

    chore: add comment

--- pages/weatherAlertPageObject.ts
+++ pages/weatherAlertPageObject.ts
@@ -5,6 +5,8 @@ export const weatherAlertPageObject = (page: Page) => {
     // I had to put a 10 second fixed wait here due to the Weather API lag (not just in Playwright but using CURL as well)
     // Lately their API is slow to respond....
     // And assertion was failing because the title was not being displayed
+    // Also every once in a while you get a CORS error, so the text isn't on the page, causing the assertion to fail
+
     // eslint-disable-next-line playwright/no-wait-for-timeout
     await page.waitForTimeout(10000);
     await expect(page.locator("weather-title")).toHaveText(title, options);
