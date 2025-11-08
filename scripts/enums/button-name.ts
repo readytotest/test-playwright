3fb0ee6aca6a137a2ad459dde12d0056fcfd2a9b
Eli <117216467+readytotest@users.noreply.github.com>
2025-11-08 09:43:41 -0800

    chore: update comment for clarity

--- scripts/enums/button-name.ts
+++ scripts/enums/button-name.ts
@@ -1,7 +1,6 @@
-// See usage in homePageObject.ts by looking for clickFeedbackWidgetSendNowButton.
 // This enforces passing an enum member, which enables autocomplete and reduces typos.
-// The function itself lives in scripts/clickButtonByName.ts, and homePage.spec.ts contains clickButtonByName,
-// which shows how it's passed as an argument.
+// The function that uses this lives in scripts/clickButtonByName.ts
+// homePage.spec.ts contains clickButtonByName, which shows how it's passed as an argument.
 
 export enum ButtonName {
   SendNow = "Send Now!",
