fe4d4bff8bdf05728f54773bffc087f10f2a505c
eli <117216467+readytotest@users.noreply.github.com>
2025-11-20 15:43:55 -0800

    chore: update comments to explain how this works to others

--- scripts/clickButtonByNameEnum.ts
+++ scripts/clickButtonByNameEnum.ts
@@ -1,6 +1,9 @@
 /*
 Example showing how to use an enum in a helper function and pass it in from the spec.
 This enforces using an enum member, which helps with autocomplete and reduces typos.
+You can see where the function is called at the link below and how the enum is passed in from the spec in homePage.spec.ts
+Don't forget you need to import the enum into the spec file to use it there as well.
+https://github.com/readytotest/test-playwright/blob/7a6271657dee76bd54fb1f5d2a0c21cc3694cc12/tests/homePage.spec.ts#L97
 */
 
 import { Page } from "@playwright/test";
--- scripts/enums/button-name.ts
+++ scripts/enums/button-name.ts
@@ -1,6 +1,6 @@
 // This enforces passing an enum member, which enables autocomplete and reduces typos.
 // The function that uses this lives in scripts/clickButtonByName.ts
-// homePage.spec.ts contains clickButtonByName, which shows how it's passed as an argument.
+// homePage.spec.ts contains clickButtonByNameEnum, which shows how it's passed as an argument.
 
 export enum ButtonName {
   SendNow = "Send Now!",
